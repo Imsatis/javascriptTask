abff79c1a7e6b64000a003b77d36240e24516f7a
imsatis <Malsonsatis@outlook.com>
2018-12-25 17:34:06 +0530

    database image add

--- Ajax/read.php
+++ Ajax/read.php
@@ -19,11 +19,21 @@ $SELECT = "SELECT * FROM USER";
 
 $result  = $conn->query($SELECT);
 
-if($result->num_rows > 0) {
-
+if($result->num_rows > 0) {$test=[];
+    // echo "<pre>";
+    // print_r($result->fetch_assoc());
+    echo"<pre>";
    while($row = $result->fetch_assoc()) {
+    array_push($test,$row);
     echo "<li onclick='read(".$row['id'].")'>".$row['name']."</li><div id='user".$row['id']."'></div>";   
    }
+   //print_r(json_encode($test));
+   $test = array(
+       'name'=>'test',
+       'username'=>'_test',
+       'email'=>'test@gmail.com'
+   );
+   print_r($test);
  }
 }
 
